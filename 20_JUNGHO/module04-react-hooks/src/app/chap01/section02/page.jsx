400f400d740c18357ab9359904c771c4b758aa46
jungho <kiik52@naver.com>
2025-05-12 16:08:58 +0900

    React - UseEffect
    - useEffect-basic
    - useEffect-dependency
    - useEffect-cleanup
    - useEffect-problem

---
+++ 20_JUNGHO/module04-react-hooks/src/app/chap01/section02/page.jsx
@@ -0,0 +1,10 @@
+import UseEffectDependency from "@/components/chap01/section02/UseEffectDependency";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <UseEffectDependency />
+    </>
+  );
+}
